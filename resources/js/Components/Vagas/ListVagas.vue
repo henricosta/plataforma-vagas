3a61e58715d97f97765c9f3f69c3feb388b60eae
henricosta <pedrocosta.lov@gmail.com>
2023-05-15 19:09:19 -0300

    create basic list vagas component

---
+++ resources/js/Components/Vagas/ListVagas.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+import CardVaga from "@/Components/Vagas/CardVaga.vue";
+
+defineProps({
+    vagas: Object
+})
+
+</script>
+
+<template>
+    <div v-for="vaga in vagas">
+        <CardVaga :titulo="vaga.titulo" :nome_empresa="vaga.nome_empresa" :descricao="vaga.descricao"/>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
