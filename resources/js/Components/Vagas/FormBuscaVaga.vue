71980c2b67ae5d8a5316f9adf790c1dafe4918b7
henricosta <pedrocosta.lov@gmail.com>
2023-05-15 20:17:26 -0300

    create basic form for querying vagas

---
+++ resources/js/Components/Vagas/FormBuscaVaga.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+<!-- TODO: Remover a borda do input quando em foco-->
+<template>
+    <form action="" class="flex justify-center">
+        <div class="py-8 flex">
+            <input type="text" name="" id="input-field" class="pl-6 placeholder-gray-400 bg-indigo-50 rounded-l-2xl border-blue-700 text-lg focus:border-transparent border-1" placeholder="Ex: Desenvolvedor Web">
+            <button type="submit" class="bg-blue-600 text-gray-50 px-4 py-4 rounded-r-2xl">Buscar vaga</button>
+        </div>
+    </form>
+</template>
+
+<style scoped>
+#input-field {
+    width: 32rem;
+}
+</style>
