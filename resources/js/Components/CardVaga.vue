288a793315cf4f35436befd9d75cd78279c5ddc0
henricosta <pedrocosta.lov@gmail.com>
2023-05-15 15:40:09 -0300

    create card vaga component

---
+++ resources/js/Components/CardVaga.vue
@@ -0,0 +1,27 @@
+<script setup>
+defineProps({
+    titulo: String,
+    empresa: Object,
+    descricao: String
+})
+
+function truncate(str, maximum_length) {
+    if (str.length > maximum_length) {
+        return str.slice(0, maximum_length) + '...'
+    }
+    return str
+}
+
+</script>
+
+<template>
+    <a href="#" class="block max-w-lg p-8 border shadow h-full">
+        <h5 class="mb-2 text-2xl">{{ truncate(titulo, 60) }}</h5>
+        <p class="mb-2 text-gray-500">{{ truncate(empresa.nome, 50) }}</p>
+        <p class="text-sm" id="card-text">{{ truncate(descricao, 120) }}</p>
+    </a>
+</template>
+
+<style scoped>
+
+</style>
