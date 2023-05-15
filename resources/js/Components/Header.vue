5bab77a1f8ad39c1d86094b8e0de3a7f618883ea
henricosta <pedrocosta.lov@gmail.com>
2023-05-15 11:00:46 -0300

    create basic header component

---
+++ resources/js/Components/Header.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+    <header>
+        <nav class="bg-blue-700 px-6 py-2">
+            <div class="flex flex-wrap justify-between text-cyan-50">
+                <div class="flex">
+                    <div class="self-center mr-10 ml-3">
+                        <button>Home</button>
+                    </div>
+                    <button class="px-2 border-b-2">Buscar vagas</button>
+                    <button class="px-2">Para empresas</button>
+                </div>
+                <div>
+                    <button class="mr-5">Login</button>
+                    <button class="border px-4 py-2 rounded hover:text-blue-700 hover:bg-gray-50 transition-colors">Register</button>
+                </div>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<style scoped>
+
+</style>
