5ada994b702ecc0037fb7c57c65baba96a73c639
henricosta <pedrocosta.lov@gmail.com>
2023-05-15 11:36:59 -0300

    add relationship in Empresa.php model

--- app/Models/Empresa.php
+++ app/Models/Empresa.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Empresa extends Model
 {
     use HasFactory;
+
+    public function vagas(): HasMany {
+        return $this->hasMany(Vaga::class);
+    }
 }
