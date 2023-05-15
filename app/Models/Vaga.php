5bf3847cccbb5c5fbfcc7a3cca200447871c1b11
henricosta <pedrocosta.lov@gmail.com>
2023-05-15 11:38:40 -0300

    add relationship in Vaga.php model

--- app/Models/Vaga.php
+++ app/Models/Vaga.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Vaga extends Model
 {
     use HasFactory;
+
+    public function empresa(): BelongsTo {
+        return $this->belongsTo(Empresa::class);
+    }
 }
