f9d7b9c20945e652fa361169430201e495c04bb4
henricosta <pedrocosta.lov@gmail.com>
2023-05-16 16:00:33 -0300

    create basic layout to display ListVagas and PageVaga

---
+++ resources/js/Layouts/LayoutVagas.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+import ListVagas from "@/Components/Vagas/ListVagas.vue";
+import PageVaga from "@/Components/Vagas/PageVaga.vue";
+import {reactive} from "vue";
+
+defineProps({
+    vagas: Object,
+})
+
+const vagaOnFocus = reactive({ vagaIndex: 0 })
+
+function updateVagaOnFocus(index) {
+    vagaOnFocus.vagaIndex = index
+}
+
+</script>
+
+<template>
+<!--    Todo: procurar uma forma de fazer com que a vaga selecionada na lista seja a que apareca na página -->
+<!--    Todo: Adicionar página de "nenhuma vaga encontrada"-->
+    <div class="flex justify-center">
+        <ListVagas :vagas="vagas" :update-vaga-focus="updateVagaOnFocus"/>
+        <PageVaga :vaga="vagas[vagaOnFocus.vagaIndex]"/>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
