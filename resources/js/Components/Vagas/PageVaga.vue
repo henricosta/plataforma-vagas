9625dc636aa6bba0c28318e03c074f36aa60ac40
henricosta <pedrocosta.lov@gmail.com>
2023-05-16 20:02:32 -0300

    make page vaga follow the scroll

--- resources/js/Components/Vagas/PageVaga.vue
+++ resources/js/Components/Vagas/PageVaga.vue
@@ -5,14 +5,14 @@ defineProps({
 </script>
 
 <template>
-<!--    Todo: Definir uma altura máxima para a página e se o texto ultrapassar, colocar uma barra de rolagem-->
-<!--    Todo: Fixar ela na janela do navegador e não permitir que a rolagem suma com a página-->
-    <div class="shadow mx-6 my-3 border rounded-3xl w-1/2">
-        <div class="m-14">
-            <h1 class="text-4xl mb-5">{{ vaga.titulo }}</h1>
-            <p class="text-gray-500">{{ vaga.nome_empresa }}</p>
-            <hr class="my-6">
-            <p>{{ vaga.descricao }}</p>
+    <div>
+        <div class="shadow mx-6 my-3 border rounded-3xl">
+            <div class="m-14">
+                <h1 class="text-4xl mb-5">{{ vaga.titulo }}</h1>
+                <p class="text-gray-500">{{ vaga.nome_empresa }}</p>
+                <hr class="my-6">
+                <p>{{ vaga.descricao }}</p>
+            </div>
         </div>
     </div>
 </template>
--- resources/js/Layouts/LayoutVagas.vue
+++ resources/js/Layouts/LayoutVagas.vue
@@ -2,7 +2,7 @@
 
 import ListVagas from "@/Components/Vagas/ListVagas.vue";
 import PageVaga from "@/Components/Vagas/PageVaga.vue";
-import {reactive} from "vue";
+import {onBeforeUnmount, onMounted, reactive, ref} from "vue";
 
 defineProps({
     vagas: Object,
@@ -14,6 +14,20 @@ function updateVagaOnFocus(index) {
     vagaOnFocus.vagaIndex = index
 }
 
+onMounted(() => {
+    window.addEventListener('scroll', handleScrolling)
+})
+onBeforeUnmount(() => {
+    window.removeEventListener('scroll', handleScrolling)
+})
+
+const isFixed = ref(false)
+
+function handleScrolling() {
+    const pageVaga = document.getElementById('page-vaga').getBoundingClientRect();
+    isFixed.value = pageVaga.height - window.scrollY < window.innerHeight;
+}
+
 </script>
 
 <template>
@@ -21,7 +35,13 @@ function updateVagaOnFocus(index) {
 <!--    Todo: Adicionar página de "nenhuma vaga encontrada"-->
     <div class="flex justify-center">
         <ListVagas :vagas="vagas" :update-vaga-focus="updateVagaOnFocus"/>
-        <PageVaga :vaga="vagas[vagaOnFocus.vagaIndex]"/>
+        <div class="w-2/4">
+            <PageVaga  id="page-vaga" :vaga="vagas[vagaOnFocus.vagaIndex]" :class="{
+                'fixed': isFixed,
+                'bottom-0': isFixed,
+                'w-2/4': isFixed
+            }"/>
+        </div>
     </div>
 </template>
 
