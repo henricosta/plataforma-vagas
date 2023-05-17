95206321fb6d52a79acc15c7b78a0ffe2712cd88
henricosta <pedrocosta.lov@gmail.com>
2023-05-17 18:40:11 -0300

    create filter local scope to filter vagas

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function scopeFilter(Builder $query, int $modalidade = null) {
+        if ($modalidade) {
+            $query->where('modalidade', '=', strval($modalidade));
+        }
+    }
 }
