dfe7b8e7ae5711d93da51fee28a566f9de0c4ab6
henricosta <pedrocosta.lov@gmail.com>
2023-05-22 16:43:26 -0300

    create placeholders for testing

--- resources/js/Components/FiltersTab.vue
+++ resources/js/Components/FiltersTab.vue
@@ -1,13 +1,27 @@
 <script setup>
 
 import Dropdown from "@/Components/Dropdown.vue";
+
+const placeholder = [
+    {
+        buttonText: 'Modalidade',
+        options: ['Presencial', 'Remoto', 'Híbrido']
+    },
+    {
+        buttonText: 'Data de publicação',
+        options: ['Hoje', 'Últimos 3 dias', 'Última semana', 'Últimos 15 dias', 'Último mês']
+    },
+    {
+        buttonText: 'Jornada',
+        options: ['Período integral', 'Parcial manhã', 'Parcial tarde', 'Parcial noite', 'Noturno']
+    },
+]
+
 </script>
 
 <template>
     <div class="border-b-2 border-t-2 py-4">
-        <Dropdown button-text="Modalidade" :options="['Presencial', 'Remoto', 'Híbrido']"/>
-        <Dropdown button-text="Data de publicação" :options="['Hoje', 'Últimos 3 dias', 'Última semana', 'Últimos 15 dias', 'Último mês']"/>
-        <Dropdown button-text="Jornada" :options="['Período integral', 'Parcial manhã', 'Parcial tarde', 'Parcial noite', 'Noturno']"/>
+        <Dropdown v-for="item in placeholder" :button-text="item.buttonText" :options="item.options"/>
     </div>
 </template>
 
