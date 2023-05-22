b9545831a5f7b7b3de579f972372979bb493b964
henricosta <pedrocosta.lov@gmail.com>
2023-05-22 08:56:06 -0300

    create basic filters tab where dropdowns are located

---
+++ resources/js/Components/FiltersTab.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+import Dropdown from "@/Components/Dropdown.vue";
+</script>
+
+<template>
+    <div class="border-b-2 border-t-2 py-4">
+        <Dropdown/>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
