38662a46c4517f9dcfb40695bc25ef8f22324f69
henricosta <pedrocosta.lov@gmail.com>
2023-05-22 16:24:57 -0300

    remote todo

--- resources/js/Components/FiltersTab.vue
+++ resources/js/Components/FiltersTab.vue
@@ -5,7 +5,9 @@ import Dropdown from "@/Components/Dropdown.vue";
 
 <template>
     <div class="border-b-2 border-t-2 py-4">
-        <Dropdown/>
+        <Dropdown button-text="Modalidade" :options="['Presencial', 'Remoto', 'Híbrido']"/>
+        <Dropdown button-text="Data de publicação" :options="['Hoje', 'Últimos 3 dias', 'Última semana', 'Últimos 15 dias', 'Último mês']"/>
+        <Dropdown button-text="Jornada" :options="['Período integral', 'Parcial manhã', 'Parcial tarde', 'Parcial noite', 'Noturno']"/>
     </div>
 </template>
 
