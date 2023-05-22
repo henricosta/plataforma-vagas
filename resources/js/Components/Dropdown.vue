3fc11e2acaa7700a4f6d17b1bd15ee5b0c5c07ff
henricosta <pedrocosta.lov@gmail.com>
2023-05-22 20:27:56 -0300

    use a computed property to update modalidade prop

--- resources/js/Components/Dropdown.vue
+++ resources/js/Components/Dropdown.vue
@@ -1,17 +1,32 @@
-<script setup lang="ts">
+<script setup>
 
-defineProps<{
-    buttonText: string,
-    options: string[]
-}>()
+import {computed} from "vue";
+
+const emit = defineEmits(['change'])
+
+const props = defineProps({
+    filter: String,
+    text: String,
+    selected: 0,
+    options: Array,
+})
+
+const selectOption = computed({
+    get() {
+        return props.selected
+    },
+    set(novoValor) {
+        emit("change", novoValor)
+    }
+})
 
 </script>
 
 <template>
     <div class="inline-block mx-4">
-        <select name="" id="">
-            <option value="" disabled selected class="hidden">{{buttonText}}</option>
-            <option v-for="item in options" value="">{{item}}</option>
+        <select :name="filter" id="" form="formulario-busca-vaga" v-model="selectOption">
+            <option value="0" disabled selected class="hidden">{{props.text}}</option>
+            <option v-for="item in props.options" :value="item.id">{{item.nome}}</option>
         </select>
     </div>
 </template>
--- resources/js/Components/Vagas/FormBuscaVaga.vue
+++ resources/js/Components/Vagas/FormBuscaVaga.vue
@@ -2,27 +2,56 @@
 
 import {router} from "@inertiajs/vue3";
 import {reactive} from "vue";
-import FiltersTab from "@/Components/FiltersTab.vue";
+import Dropdown from "@/Components/Dropdown.vue";
+
+const placeholder = [
+    {
+        filter: 'modalidade',
+        text: 'Modalidade',
+        options: [
+            {
+                id: 1,
+                nome: 'Presencial'
+            },
+            {
+                id: 2,
+                nome: 'Remoto'
+            },
+            {
+                id: 3,
+                nome: 'Híbrido'
+            }
+        ]
+    }
+]
 
 const form = reactive({
-    titulo: null
+    busca: null,
+    modalidade: 0
 })
 
 function submit() {
     router.get('/vagas/busca', form)
 }
 
+function updateModalidade(value) {
+    form.modalidade = value
+}
+
 </script>
 <!-- TODO: Enviar filtros junto com o input -->
 <template>
-    <form @submit.prevent="submit">
+    <h1>{{form.modalidade}}</h1>
+    <form @submit.prevent="submit" id="formulario-busca-vaga">
         <div class="flex justify-center">
             <div class="py-8 flex">
-                <input type="text" id="input-field" class="pl-6 placeholder-gray-400 bg-indigo-50 rounded-l-2xl border-blue-700 text-lg focus:border-transparent border-1" placeholder="Ex: Desenvolvedor Web" v-model="form.titulo">
+                <input type="text" id="input-field" class="pl-6 placeholder-gray-400 bg-indigo-50 rounded-l-2xl border-blue-700 text-lg focus:border-transparent border-1" placeholder="Ex: Desenvolvedor Web" v-model="form.search">
                 <button type="submit" class="bg-blue-600 text-gray-50 px-4 py-4 rounded-r-2xl">Buscar vaga</button>
             </div>
         </div>
-        <FiltersTab />
+        <div class="border-b-2 border-t-2 py-4">
+            <Dropdown @change="updateModalidade" :selected="form.modalidade" v-for="item in placeholder" :filter="item.filter" :text="item.text" :options="item.options"/>
+        </div>
     </form>
 </template>
 
