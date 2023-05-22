c07657fa54237b921fe83bcaa9cc8a41497b4aab
henricosta <pedrocosta.lov@gmail.com>
2023-05-22 16:22:36 -0300

    add filter list to form

--- resources/js/Components/Vagas/FormBuscaVaga.vue
+++ resources/js/Components/Vagas/FormBuscaVaga.vue
@@ -2,6 +2,7 @@
 
 import {router} from "@inertiajs/vue3";
 import {reactive} from "vue";
+import FiltersTab from "@/Components/FiltersTab.vue";
 
 const form = reactive({
     titulo: null
@@ -14,11 +15,14 @@ function submit() {
 </script>
 <!-- TODO: Remover a borda do input quando em foco-->
 <template>
-    <form @submit.prevent="submit" class="flex justify-center">
-        <div class="py-8 flex">
-            <input type="text" id="input-field" class="pl-6 placeholder-gray-400 bg-indigo-50 rounded-l-2xl border-blue-700 text-lg focus:border-transparent border-1" placeholder="Ex: Desenvolvedor Web" v-model="form.titulo">
-            <button type="submit" class="bg-blue-600 text-gray-50 px-4 py-4 rounded-r-2xl">Buscar vaga</button>
+    <form @submit.prevent="submit">
+        <div class="flex justify-center">
+            <div class="py-8 flex">
+                <input type="text" id="input-field" class="pl-6 placeholder-gray-400 bg-indigo-50 rounded-l-2xl border-blue-700 text-lg focus:border-transparent border-1" placeholder="Ex: Desenvolvedor Web" v-model="form.titulo">
+                <button type="submit" class="bg-blue-600 text-gray-50 px-4 py-4 rounded-r-2xl">Buscar vaga</button>
+            </div>
         </div>
+        <FiltersTab />
     </form>
 </template>
 
