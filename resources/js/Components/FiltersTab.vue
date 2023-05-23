2bcbbbba02244de24573187448f852ffd527cda5
henricosta <pedrocosta.lov@gmail.com>
2023-05-23 12:14:28 -0300

    Revert "remove useless div"
    This reverts commit b2fd42877b8b6ddd15ef93aa70a04a0afbc664d9.

--- resources/js/Components/FiltersTab.vue
+++ resources/js/Components/FiltersTab.vue
@@ -2,15 +2,26 @@
 
 import Dropdown from "@/Components/Dropdown.vue";
 
-defineProps({
-    dropdownItems: Array
-})
+const placeholder = [
+    {
+        buttonText: 'Modalidade',
+        options: ['Presencial', 'Remoto', 'Híbrido']
+    },
+    {
+        buttonText: 'Data de publicação',
+        options: ['Hoje', 'Últimos 3 dias', 'Última semana', 'Últimos 15 dias', 'Último mês']
+    },
+    {
+        buttonText: 'Jornada',
+        options: ['Período integral', 'Parcial manhã', 'Parcial tarde', 'Parcial noite', 'Noturno']
+    },
+]
 
 </script>
 
 <template>
     <div class="border-b-2 border-t-2 py-4">
-        <Dropdown v-for="item in dropdownItems" :filter="item.filter" :text="item.text" :options="item.options"/>
+        <Dropdown v-for="item in placeholder" :button-text="item.buttonText" :options="item.options"/>
     </div>
 </template>
 
