baa2a28fec990514c58502b9fe8df74eb682d39f
henricosta <pedrocosta.lov@gmail.com>
2023-05-24 09:02:30 -0300

    fix scroll

--- resources/js/Components/Vagas/CardVaga.vue
+++ resources/js/Components/Vagas/CardVaga.vue
@@ -16,11 +16,11 @@ function truncate(str, maximum_length) {
 </script>
 
 <template>
-    <a href="#" class="block max-w-lg p-8 border shadow h-full rounded-3xl my-3 mx-5">
+    <div class="block max-w-lg p-8 border shadow h-full rounded-3xl my-3 mx-5">
         <h5 class="mb-2 text-2xl">{{ truncate(titulo, 50) }}</h5>
         <p class="mb-2 text-gray-500">{{ truncate(nome_empresa, 30) }}</p>
         <p class="text-sm" id="card-text">{{ truncate(descricao, 130) }}</p>
-    </a>
+    </div>
 </template>
 
 <style scoped>
