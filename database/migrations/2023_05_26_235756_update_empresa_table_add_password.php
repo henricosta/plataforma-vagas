97c6b919af2c7d2363e87ecb0eb11c7897ac65c6
henricosta <pedrocosta.lov@gmail.com>
2023-05-27 12:10:15 -0300

    fix typo

--- database/migrations/2023_05_26_235756_update_empresa_table_add_password.php
+++ database/migrations/2023_05_26_235756_update_empresa_table_add_password.php
@@ -23,7 +23,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('empresas', function(Blueprint $table) {
-            $table->dropColumn('passoword');
+            $table->dropColumn('password');
         });
         Schema::table('empresas', function(Blueprint $table) {
             $table->dropColumn('remember_token');
