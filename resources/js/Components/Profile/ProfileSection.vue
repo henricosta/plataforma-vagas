62a4bc77373134e4c75728d2a159aff9d51f87c6
henricosta <pedrocosta.lov@gmail.com>
2023-05-29 14:00:25 -0300

    create components for profile sections

---
+++ resources/js/Components/Profile/ProfileSection.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Section from '../Section.vue';
+
+defineProps({
+    nome_completo: String,
+    email: String,
+    telefone: String
+})
+</script>
+
+<template>
+    <Section>
+        <div
+            class="mb-8 relative inline-flex items-center justify-center w-60 h-60 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+            <span class="font-medium text-gray-600 dark:text-gray-300">JL</span>
+        </div>
+        <div>
+            <div class="mb-3 text-4xl font-extrabold dark:text-white">{{ nome_completo }}</div>
+            <ul class="max-w-md space-y-1 text-gray-500 list-none list-inside dark:text-gray-400">
+                <li>Email: {{ email }}</li>
+                <li v-if="telefone">Telefone: {{ telefone }}</li>
+            </ul>
+        </div>
+    </Section>
+</template>
---
+++ resources/js/Components/Profile/ProfileSectionEmpresa.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Section from '../Section.vue';
+
+defineProps({
+    nome: String,
+})
+</script>
+
+<template>
+    <Section>
+        <div
+            class="mb-8 relative inline-flex items-center justify-center w-60 h-60 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+            <span class="font-medium text-gray-600 dark:text-gray-300">Empresa</span>
+        </div>
+        <div>
+            <div class="mb-3 text-4xl font-extrabold dark:text-white">{{ nome }}</div>
+        </div>
+    </Section>
+</template>
