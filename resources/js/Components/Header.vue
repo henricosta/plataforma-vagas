f12bcf64c376a3dec7e40e4c524d9cedd4aed97b
henricosta <pedrocosta.lov@gmail.com>
2023-05-31 17:43:14 -0300

    update header

--- resources/js/Components/Header.vue
+++ resources/js/Components/Header.vue
@@ -1,37 +1,62 @@
 <script setup>
+import ResponsiveNavLink from './ResponsiveNavLink.vue';
+import NavLink from './NavLink.vue';
+import SecondaryButton from './SecondaryButton.vue';
+import PrimaryButton from './PrimaryButton.vue';
 import { Link } from '@inertiajs/vue3';
 
-defineProps({
-    isLogged: false
+const props = defineProps({
+    isLogged: {
+        type: Boolean,
+        default: false
+    }
 })
 
 </script>
 
 <template>
     <header>
-        <nav class="bg-blue-700 px-6 py-2">
-            <div class="flex flex-wrap justify-between text-cyan-50">
+        <nav class="bg-white border-b border-gray-100">
+            <div class="flex flex-wrap justify-between text-gray-800 h-16">
                 <div class="flex">
-                    <div class="self-center mr-10 ml-3">
-                        <a href="/">Home</a>
+                    <!-- Logo -->
+                    <div class="flex shrink-0 items-center mx-10">
+                        <Link href="/" class="text-gray-900">
+                        HOME
+                        </Link>
                     </div>
-                    <div class="self-center">
-                        <button class="px-2">Buscar vagas</button>
-                        <button class="px-2">Para empresas</button>
+                    <!-- Navigation Links -->
+                    <div class="flex items-center">
+                        <Link href="/" class="px-2">Buscar vagas</Link>
+                        <Link v-if="props.isLogged" href="/profile/vagas" class="px-2">Minhas vagas</Link>
+                        <Link v-else href="/empresa" class="px-2">Para empresas</Link>
                     </div>
                 </div>
-                <div v-if="!isLogged">
-                    <Link href="/login"><button class="mr-5">Login</button></Link>
-                    <Link href="/register"><button class="border px-4 py-2 rounded hover:text-blue-700 hover:bg-gray-50 transition-colors">Register</button></Link>
-                </div>
-                <div v-else>
-                    <Link href="/profile"><button class="border px-4 py-2 rounded hover:text-blue-700 hover:bg-gray-50 transition-colors">Perfil</button></Link>
+                <!-- Botões da direita -->
+                <div class="flex items-center mr-8">
+                    <div v-if="props.isLogged">
+                        <Link href="/profile">
+
+                        <div class="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+                            <svg class="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                                    clip-rule="evenodd"></path>
+                            </svg>
+                        </div>
+
+                        </Link>
+                    </div>
+                    <div v-else>
+                        <Link href="/login" class="mr-5">Entrar</Link>
+                        <Link href="/register">
+                            <PrimaryButton class="py-3">Cadastre-se</PrimaryButton>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </nav>
     </header>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
