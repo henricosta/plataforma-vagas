49a13f0726f8e22784c20c73be71eaaa3d420117
henricosta <pedrocosta.lov@gmail.com>
2023-05-31 20:03:13 -0300

    update page vaga

--- resources/js/Components/Vagas/PageVaga.vue
+++ resources/js/Components/Vagas/PageVaga.vue
@@ -1,22 +1,30 @@
 <script setup>
+import PrimaryButton from '../PrimaryButton.vue';
+
 defineProps({
     vaga: Object,
 })
+
+// TODO: Implementar função
+const candidatar = () => {
+
+}
 </script>
 
 <template>
     <div>
-        <div class="shadow mx-6 my-3 border rounded-3xl">
+        <div class="bg-white shadow mx-6 my-3 border rounded-lg">
             <div class="m-14">
-                <h1 class="text-4xl mb-5">{{ vaga.titulo }}</h1>
-                <p class="text-gray-500">{{ vaga.nome_empresa }}</p>
-                <hr class="my-6">
+                <h2 class="text-3xl mb-2">{{ vaga.titulo }}</h2>
+                <p class="text-gray-800">{{ vaga.empresa.nome }} - {{ vaga.nome_cidade }} ({{ modalidade == 1 ? 'Presencial' :
+                    modalidade == 2 ? 'Híbrido' : 'Remoto' }})</p>
+                <PrimaryButton class="px-2 py-3 mt-4">Candidatar-se</PrimaryButton>
+                <p class="mt-5"><strong>Descrição da vaga</strong></p>
+                <hr class="my-1">
                 <p>{{ vaga.descricao }}</p>
             </div>
         </div>
     </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
