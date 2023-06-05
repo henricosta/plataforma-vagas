000a8147996b99c26c2614fb2ffecdbc03fe027b
henricosta <pedrocosta.lov@gmail.com>
2023-06-05 19:51:45 -0300

    format telefone input

--- resources/js/Components/FormEmpresa.vue
+++ resources/js/Components/FormEmpresa.vue
@@ -4,41 +4,72 @@ import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
 import { Link, useForm } from '@inertiajs/vue3';
+import { ref, computed } from 'vue';
 
-// TODO: Mudar controller para compreender essa mudança
-// nome -> nome_completo, nome_empresa
 const form = useForm({
-    nome_completo: '',
+    nome_titular: '',
     nome_empresa: '',
     cnpj: '',
     cep: '',
     email: '',
-    num_funcionarios: null,
+    telefone: '',
+    num_funcionarios: 0,
     password: '',
     password_confirmation: '',
 });
 
 const submit = () => {
-    form.post(route('empresa.register'), {
-        onFinish: () => form.reset('password', 'password_confirmation'),
-    });
+    form.telefone = telefone
+    form.post(route('empresa.register'));
 };
+
+const telefone = ref('');
+
+function formatTelefone() {
+    const rawNumber = telefone.value.replace(/\D/g, ""); // Remove non-digit characters
+    let formattedNumber = "";
+
+    if (rawNumber.length > 0) {
+        formattedNumber = rawNumber
+        
+        if (rawNumber.length > 2) {
+            formattedNumber = "(" + rawNumber.substring(0, 2) + ")"; // Add area code
+
+            if (rawNumber.length > 2) {
+                formattedNumber += " " + rawNumber.substring(2, 3); // Add first digit after area code
+
+                if (rawNumber.length > 3) {
+                    formattedNumber += "" + rawNumber.substring(3, 7); // Add first four digits
+
+                    if (rawNumber.length > 7) {
+                        formattedNumber += "-" + rawNumber.substring(7); // Add remaining digits
+                    }
+                }
+            }
+        }
+    }
+
+    telefone.value = formattedNumber;
+}
+
+
+
 </script>
 
 <template>
     <div class="bg-white px-6 py-4 shadow-md overflow-hidden rounded-lg w-1/2 mt-5">
         <div>
-            <h1 class="text-3xl mb-6" id="titulo" >Cadastre a sua empresa</h1>
+            <h1 class="text-3xl mb-6" id="titulo">Cadastre a sua empresa</h1>
         </div>
         <form @submit.prevent="submit">
             <!-- Nome da empresa -->
             <div>
-                <InputLabel for="nome_completo" value="Seu nome completo" />
+                <InputLabel for="nome_titular" value="Seu nome completo" />
 
-                <TextInput id="nome_completo" type="text" class="mt-1 block w-full" v-model="form.nome_completo" required
-                    autofocus autocomplete="nome_completo" />
+                <TextInput id="nome_titular" type="text" class="mt-1 block w-full" v-model="form.nome_titular" required
+                    autofocus autocomplete="nome_titular" />
 
-                <InputError class="mt-2" :message="form.errors.nome_completo" />
+                <InputError class="mt-2" :message="form.errors.nome_titular" />
             </div>
             <!-- Nome do titular da empresa -->
             <div class="mt-4">
@@ -49,31 +80,40 @@ const submit = () => {
 
                 <InputError class="mt-2" :message="form.errors.nome_empresa" />
             </div>
-            <!-- Agrupa os inputs de CNPJ e CEP -->
+            <!-- Agrupa os inputs de CNPJ e CEP e numero de funcionarios-->
             <div class="flex justify-between mt-4">
                 <!-- CNPJ da empresa -->
-                <div style="width: 45%;">
-                    <InputLabel for="cnpj" value="CNPJ da empresa" />
+                <div style="width: 35%;">
+                    <InputLabel for="cnpj" value="CNPJ" />
 
-                    <TextInput id="cnpj" type="text" class="mt-1 block w-full" v-model="form.cnpj" required
-                        autofocus autocomplete="cnpj" />
+                    <TextInput id="cnpj" type="text" class="mt-1 block w-full" v-model="form.cnpj" required autofocus
+                        autocomplete="cnpj" />
 
                     <InputError class="mt-2" :message="form.errors.cnpj" />
                 </div>
                 <!-- CEP da empresa -->
-                <div style="width: 45%;">
-                    <InputLabel for="cep" value="CEP da empresa" />
+                <div style="width: 25%;">
+                    <InputLabel for="cep" value="CEP" />
 
-                    <TextInput id="cep" type="text" class="mt-1 block w-full" v-model="form.cep" required
-                        autofocus autocomplete="cep" />
+                    <TextInput id="cep" type="text" class="mt-1 block w-full" v-model="form.cep" required autofocus
+                        autocomplete="cep" />
 
                     <InputError class="mt-2" :message="form.errors.cep" />
                 </div>
+                <!-- Número de funcionarios da empresa -->
+                <div style="width: 30%">
+                    <InputLabel for="numero_funcionarios" value="Funcionários" />
+
+                    <TextInput id="numero_vagas" type="number" class="mt-1 block w-full" v-model="form.num_funcionarios"
+                        required autofocus autocomplete="username" />
+
+                    <InputError class="mt-2" :message="form.errors.num_funcionarios" />
+                </div>
             </div>
-            <!-- Agrupa os inputs de Emails e Funcionarios -->
-            <div class="flex justify-between">
+            <!-- Agrupa os inputs de Email, Telefone e Funcionarios -->
+            <div class="flex justify-between mt-4">
                 <!-- Email corporativo da empresa -->
-                <div class="mt-4" style="width: 60%;">
+                <div style="width: 60%;">
                     <InputLabel for="email" value="Email corporativo" />
 
                     <TextInput id="email" type="email" class="mt-1 block w-full" v-model="form.email" required
@@ -81,21 +121,17 @@ const submit = () => {
 
                     <InputError class="mt-2" :message="form.errors.email" />
                 </div>
-                <!-- Número de funcionarios da empresa -->
-                <div class="mt-4" style="width: 30%">
-                    <InputLabel for="numero_funcionarios" value="Número de funcionários" />
+                <!-- Telefone -->
+                <div style="width: 35%">
+                    <InputLabel for="telefone" value="Telefone" />
 
-                    <TextInput
-                        id="numero_vagas"
-                        type="number"
-                        class="mt-1 block w-full"
-                        v-model="form.num_funcionarios"
-                        required
-                        autofocus
-                        autocomplete="username"
-                    />
+                    <input
+                        class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm mt-1 w-full"
+                        type="text" id="telefone" placeholder="(99) 9999-9999" v-model="telefone"
+                        @input="formatTelefone" 
+                        @change="formatTelefone" />
 
-                    <InputError class="mt-2" :message="form.errors.titulo" />
+                    <InputError class="mt-2" :message="form.errors.telefone" />
                 </div>
             </div>
             <!-- Senha e confimação de senha -->
