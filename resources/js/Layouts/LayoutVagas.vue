2109909452834d55888b9f85db21cff56bb602cc
henricosta <pedrocosta.lov@gmail.com>
2023-06-09 10:44:35 -0300

    add component for when vagas< 1

--- resources/js/Layouts/LayoutVagas.vue
+++ resources/js/Layouts/LayoutVagas.vue
@@ -2,10 +2,11 @@
 
 import ListVagas from "@/Components/Vagas/ListVagas.vue";
 import PageVaga from "@/Components/Vagas/PageVaga.vue";
-import { onBeforeUnmount, onMounted, reactive, ref } from "vue";
+import { onBeforeUnmount, onMounted, reactive, ref, watch } from "vue";
+import Section from "@/Components/Section.vue";
 
-defineProps({
-    vagas: Object,
+const props = defineProps({
+    vagas: Array,
     previousPageUrl: String,
     nextPageUrl: String,
     totalPages: Number,
@@ -19,10 +20,14 @@ function updateVagaOnFocus(index) {
 }
 
 onMounted(() => {
-    window.addEventListener('scroll', handleScrolling)
+    if (props.vagas.length) window.addEventListener('scroll', handleScrolling)
 })
 onBeforeUnmount(() => {
     window.removeEventListener('scroll', handleScrolling)
+})
+
+watch(() => props.vagas, (newVagas) => {
+    if (!newVagas.length) window.removeEventListener('scroll', handleScrolling)
 })
 
 const isFixed = ref(false)
@@ -36,16 +41,21 @@ function handleScrolling() {
 
 <template>
     <!--    TODO: Adicionar página de "nenhuma vaga encontrada"-->
-    <div class="flex justify-center">
-        <ListVagas :vagas="vagas" :update-vaga-focus="updateVagaOnFocus" :next-page-url="nextPageUrl"
-            :previous-page-url="previousPageUrl" :total-pages="totalPages" :current-page="currentPage"/>
+    <div v-if="props.vagas.length" class="flex justify-center">
+        <ListVagas :vagas="props.vagas" :update-vaga-focus="updateVagaOnFocus" :next-page-url="props.nextPageUrl"
+            :previous-page-url="props.previousPageUrl" :total-pages="props.totalPages" :current-page="props.currentPage"/>
         <div class="w-2/4">
-            <PageVaga id="page-vaga" :vaga="vagas[vagaOnFocus.vagaIndex]" :class="{
+            <PageVaga id="page-vaga" :vaga="props.vagas[vagaOnFocus.vagaIndex]" :class="{
                 'fixed': isFixed,
                 'bottom-0': isFixed,
                 'w-2/4': isFixed
             }" />
         </div>
+    </div>
+    <div v-else class="flex justify-center mt-12">
+        <Section class="w-1/2 flex justify-center text-gray-800">
+            <h1 class="text-3xl">Nenhuma vaga encontrada</h1>
+        </Section>
     </div>
 </template>
 
