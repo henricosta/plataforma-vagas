9c8dde68e550e5cac661b8e34454bab7380af7fe
henricosta <pedrocosta.lov@gmail.com>
2023-06-10 09:14:07 -0300

    fix broken section controller

--- resources/js/Components/Section.vue
+++ resources/js/Components/Section.vue
@@ -1,12 +1,12 @@
 <script setup>
 defineProps({
-    titulo: ''
+    titulo: String
 })
 </script>
 
 <template>
     <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-        <h4 v-if="titulo != ''" class="inline-block mb-2 text-lg font-semibold text-gray-900 dark:text-white">{{ titulo }}</h4>
+        <h4 v-if="titulo" class="inline-block mb-2 text-lg font-semibold text-gray-900 dark:text-white">{{ titulo }}</h4>
         <slot />
     </div>
 </template>
