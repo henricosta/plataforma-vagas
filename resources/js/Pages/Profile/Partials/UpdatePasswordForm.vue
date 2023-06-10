217f69ef7eeb9f701fe89f0b40869c761d22f31b
henricosta <pedrocosta.lov@gmail.com>
2023-06-10 13:29:47 -0300

    traducao

--- resources/js/Pages/Profile/Partials/UpdatePasswordForm.vue
+++ resources/js/Pages/Profile/Partials/UpdatePasswordForm.vue
@@ -36,11 +36,7 @@ const updatePassword = () => {
 <template>
     <section>
         <header>
-            <h2 class="text-lg font-medium text-gray-900">Update Password</h2>
-
-            <p class="mt-1 text-sm text-gray-600">
-                Ensure your account is using a long, random password to stay secure.
-            </p>
+            <h2 class="text-lg font-medium text-gray-900">Atualizar senha</h2>
         </header>
 
         <form @submit.prevent="updatePassword" class="mt-6 space-y-6">
