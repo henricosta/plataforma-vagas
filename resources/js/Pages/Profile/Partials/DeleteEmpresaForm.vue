3b09d154e75adfea6507a6e4673671b23ca3318f
henricosta <pedrocosta.lov@gmail.com>
2023-06-11 12:15:04 -0300

    create delete empresa form component

---
+++ resources/js/Pages/Profile/Partials/DeleteEmpresaForm.vue
@@ -0,0 +1,85 @@
+<script setup>
+import DangerButton from '@/Components/DangerButton.vue';
+import InputError from '@/Components/InputError.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import Modal from '@/Components/Modal.vue';
+import SecondaryButton from '@/Components/SecondaryButton.vue';
+import TextInput from '@/Components/TextInput.vue';
+import { useForm } from '@inertiajs/vue3';
+import { nextTick, ref } from 'vue';
+
+const confirmingEmpresaDeletion = ref(false);
+const passwordInput = ref(null);
+
+const form = useForm({
+    password: '',
+});
+
+const confirmUserDeletion = () => {
+    confirmingEmpresaDeletion.value = true;
+
+    nextTick(() => passwordInput.value.focus());
+};
+
+const deleteUser = () => {
+    form.delete(route('empresa.destroy'), {
+        preserveScroll: true,
+        onSuccess: () => closeModal(),
+        onError: () => passwordInput.value.focus(),
+        onFinish: () => form.reset(),
+    });
+};
+
+const closeModal = () => {
+    confirmingEmpresaDeletion.value = false;
+
+    form.reset();
+};
+</script>
+
+<template>
+    <section class="space-y-6">
+        <DangerButton @click="confirmUserDeletion">Apagar conta</DangerButton>
+
+        <Modal :show="confirmingEmpresaDeletion" @close="closeModal">
+            <div class="p-6">
+                <h2 class="text-lg font-medium text-gray-900">
+                    Tem certeza que quer deletar sua empresa?
+                </h2>
+
+                <p class="mt-1 text-sm text-gray-600">
+                    Insira sua senha para confirmar que você gostaria de apagar permanentemente a sua conta.
+                </p>
+
+                <div class="mt-6">
+                    <InputLabel for="password" value="Senha" class="sr-only" />
+
+                    <TextInput
+                        id="password"
+                        ref="passwordInput"
+                        v-model="form.password"
+                        type="password"
+                        class="mt-1 block w-3/4"
+                        placeholder="Senha"
+                        @keyup.enter="deleteUser"
+                    />
+
+                    <InputError :message="form.errors.password" class="mt-2" />
+                </div>
+
+                <div class="mt-6 flex justify-end">
+                    <SecondaryButton @click="closeModal"> Cancelar </SecondaryButton>
+
+                    <DangerButton
+                        class="ml-3"
+                        :class="{ 'opacity-25': form.processing }"
+                        :disabled="form.processing"
+                        @click="deleteUser"
+                    >
+                        Apagar empresa
+                    </DangerButton>
+                </div>
+            </div>
+        </Modal>
+    </section>
+</template>
