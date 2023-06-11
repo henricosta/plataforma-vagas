c8939d53e36593928032ca8636021ed24ea89abb
henricosta <pedrocosta.lov@gmail.com>
2023-06-11 15:47:35 -0300

    fix profile image and placeholder rendering at the same time

--- resources/js/Components/UserHeader.vue
+++ resources/js/Components/UserHeader.vue
@@ -31,7 +31,7 @@ const props = defineProps({
                             <div v-if="props.profile_image" class="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
                                 <img :src="props.profile_image" alt="profile image">
                             </div>
-                            <div class="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+                            <div v-else class="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
                                 <svg class="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20"
                                     xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
