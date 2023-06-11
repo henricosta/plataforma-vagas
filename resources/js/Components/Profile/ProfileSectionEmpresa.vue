430fed01b082f54ad28ffeb6fc572c5498d36dde
henricosta <pedrocosta.lov@gmail.com>
2023-06-11 12:50:55 -0300

    link to edit profile page

--- resources/js/Components/Profile/ProfileSectionEmpresa.vue
+++ resources/js/Components/Profile/ProfileSectionEmpresa.vue
@@ -1,5 +1,6 @@
 <script setup>
 import Section from '../Section.vue';
+import { Link } from '@inertiajs/vue3';
 
 defineProps({
     nome: String,
@@ -12,8 +13,9 @@ defineProps({
             class="mb-8 relative inline-flex items-center justify-center w-60 h-60 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
             <span class="font-medium text-gray-600 dark:text-gray-300">Empresa</span>
         </div>
-        <div>
-            <div class="mb-3 text-4xl font-extrabold dark:text-white">{{ nome }}</div>
+        <div class="flex items-center">
+            <div class="mb-3 text-4xl font-extrabold dark:text-white" >{{nome}}</div>
+            <Link :href="route('empresa.edit')" class="ml-4 mb-2 py-2 px-3 rounded-md border bg-gray-200 shadow-sm text-sm hover:bg-gray-400">Editar informações</Link>
         </div>
     </Section>
 </template>
