c1cc6e3b44b50876656308b69dc14363d00334f6
henricosta <pedrocosta.lov@gmail.com>
2023-06-12 14:37:16 -0300

    add link vaga route

--- resources/js/Components/SmallVagaCard.vue
+++ resources/js/Components/SmallVagaCard.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { computed } from '@vue/reactivity';
+import { Link } from '@inertiajs/vue3';
 
 const props = defineProps({
+    id: Number,
     titulo: String,
     nome_empresa: String,
     descricao: String,
@@ -31,11 +33,13 @@ function truncate(str, maximum_length) {
 </script>
 
 <template>
-    <div class="w-96 mb-3 border rounded-md shadow-sm p-4 cursor-pointer hover:bg-gray-50">
-        <h1 class="text-lg"><strong>{{ truncate(props.titulo, 40) }}</strong></h1>
-        <p class="text-sm text-gray-600">{{ props.nome_empresa ? props.nome_empresa + ' - ' : '' }}{{ props.cidade }} ({{ computedModalidade }})</p>
-        <p class="mt-2 text-sm text-gray-600">{{ truncate(props.descricao, 50) }}</p>
-    </div>
+    <Link :href="route('get.vaga', props.id)">
+        <div class="w-96 mb-3 border rounded-md shadow-sm p-4 cursor-pointer hover:bg-gray-50">
+            <h1 class="text-lg"><strong>{{ truncate(props.titulo, 40) }}</strong></h1>
+            <p class="text-sm text-gray-600">{{ props.nome_empresa ? props.nome_empresa + ' - ' : '' }}{{ props.cidade }} ({{ computedModalidade }})</p>
+            <p class="mt-2 text-sm text-gray-600">{{ truncate(props.descricao, 50) }}</p>
+        </div>
+    </Link>
 </template>
 
 <style scoped>
