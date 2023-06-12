ca8f14476a2c625788e0e933ce1180776a61245f
henricosta <pedrocosta.lov@gmail.com>
2023-06-12 10:25:08 -0300

    update small vaga card to accept empty empresa field

--- resources/js/Components/SmallVagaCard.vue
+++ resources/js/Components/SmallVagaCard.vue
@@ -33,7 +33,7 @@ function truncate(str, maximum_length) {
 <template>
     <div class="w-96 border rounded-md shadow-sm p-4 cursor-pointer hover:bg-gray-50">
         <h1 class="text-lg"><strong>{{ truncate(props.titulo, 40) }}</strong></h1>
-        <p class="text-sm text-gray-600">{{ props.nome_empresa }} - {{ props.cidade }} ({{ computedModalidade }})</p>
+        <p class="text-sm text-gray-600">{{ props.nome_empresa ? props.nome_empresa + ' - ' : '' }}{{ props.cidade }} ({{ computedModalidade }})</p>
         <p class="mt-2 text-sm text-gray-600">{{ truncate(props.descricao, 50) }}</p>
     </div>
 </template>
