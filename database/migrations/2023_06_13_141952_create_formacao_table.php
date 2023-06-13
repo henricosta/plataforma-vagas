cc004df526382aa47a26f5186ce05608c1e2ea1b
henricosta <pedrocosta.lov@gmail.com>
2023-06-13 15:12:00 -0300

    change from foreign key to bigint

--- database/migrations/2023_06_13_141952_create_formacao_table.php
+++ database/migrations/2023_06_13_141952_create_formacao_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('formacao', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->bigInteger('user_id');
             $table->string('instituicao');
             $table->string('diploma');
             $table->string('area');
