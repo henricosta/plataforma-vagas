3532fb1166c6eff01266320e8a5637710fa48c43
henricosta <pedrocosta.lov@gmail.com>
2023-06-13 09:59:19 -0300

    create layout with two columns

---
+++ resources/js/Layouts/Columnlayout.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="py-12">
+            <div class="flex justify-around">
+                <!-- Coluna maior da esquerda -->
+                <div class="pl-8 space-y-6 flex-grow mr-8">
+                    <slot name="esquerda"></slot>
+                </div>
+                <!-- Coluna da direita -->
+                <div class="mr-8 w-96">
+                    <slot name="direita"></slot>
+                </div>
+            </div>
+        </div>
+</template>
