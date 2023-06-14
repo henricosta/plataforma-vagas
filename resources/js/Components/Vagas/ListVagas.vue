ec04315394996c22c70982187421297b227bdbdf
henricosta <pedrocosta.lov@gmail.com>
2023-06-14 09:01:40 -0300

    set height for vagas list

--- resources/js/Components/Vagas/ListVagas.vue
+++ resources/js/Components/Vagas/ListVagas.vue
@@ -20,17 +20,20 @@ function getPageLink(i) {
 
 <template>
     <div class="w-2/5">
-        <!-- A key desse v-for não pode ser o id da vaga, mas sim o index
-                            para a funcionalidade de selecionar vaga funcionar da forma correta.
-                        -->
-        <div v-for="(vaga, index) in props.vagas" :key="index">
-            <CardVaga :titulo="vaga.titulo" :nome_empresa="vaga.empresa.nome_empresa" :descricao="vaga.descricao"
-                :num_vagas="vaga.num_vagas" :modalidade="vaga.modalidade" :cidade="vaga.nome_cidade"
-                @click="updateVagaFocus(index)" />
+        <div class="lista">
+            <!-- A key desse v-for não pode ser o id da vaga, mas sim o index
+                                para a funcionalidade de selecionar vaga funcionar da forma correta.
+                            -->
+            <div v-for="(vaga, index) in props.vagas" :key="index">
+                <CardVaga :titulo="vaga.titulo" :nome_empresa="vaga.empresa.nome_empresa" :descricao="vaga.descricao"
+                    :num_vagas="vaga.num_vagas" :modalidade="vaga.modalidade" :cidade="vaga.nome_cidade"
+                    @click="updateVagaFocus(index)" />
+            </div>
         </div>
+        <!-- Botões de navegação -->
         <div class="flex justify-center -ml-2">
             <div class="flex justify-between w-1/2 mt-4 mb-8">
-                <!-- Botão de página anterior -->
+                
                 <Link :href="previousPageUrl"
                     class="text-white bg-gray-900 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24"
@@ -39,20 +42,22 @@ function getPageLink(i) {
                 </svg>
                 <span class="sr-only">Icon description</span>
                 </Link>
-                <!-- Número e botões de página -->
-                <ul v-if="totalPages > 1" class="flex items-center">
-                    <li v-if="totalPages > 5" v-for="i in 5" class="px-1">
-                        <div class="flex align-middle justify-center items-center w-7 h-7 rounded-full" :class="{'bg-gray-400': currentPage === i}">
-                            <Link :href="getPageLink(i)">{{ i }}</Link>
-                        </div>
-                    </li>
-                    <li v-else v-for="i in totalPages" class="px-1">
-                        <div class="flex align-middle justify-center items-center w-7 h-7 rounded-full" :class="{'bg-gray-400': currentPage === i}">
-                            <Link :href="getPageLink(i)">{{ i }}</Link>
-                        </div>
-                    </li>
-                </ul>
-                <!-- Botão de próxima página -->
+                
+                <div v-if="vagas.length > 10">
+                    <ul v-if="totalPages > 1" class="flex items-center">
+                        <li v-if="totalPages > 5" v-for="i in 5" class="px-1">
+                            <div class="flex align-middle justify-center items-center w-7 h-7 rounded-full" :class="{'bg-gray-400': currentPage === i}">
+                                <Link :href="getPageLink(i)">{{ i }}</Link>
+                            </div>
+                        </li>
+                        <li v-else v-for="i in totalPages" class="px-1">
+                            <div class="flex align-middle justify-center items-center w-7 h-7 rounded-full" :class="{'bg-gray-400': currentPage === i}">
+                                <Link :href="getPageLink(i)">{{ i }}</Link>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                
                 <Link :href="nextPageUrl" type="button"
                     class="text-white bg-gray-900 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24"
@@ -66,4 +71,9 @@ function getPageLink(i) {
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.lista {
+    max-height: 145vh;
+    overflow: auto
+}
+</style>
